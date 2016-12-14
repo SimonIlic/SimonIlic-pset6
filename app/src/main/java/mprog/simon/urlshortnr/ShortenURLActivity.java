d577ba1758ac1b3c50ea092b077c824d260ec541
SimonIlic <simon.ilic@student.uva.nl>
2016-12-14 22:49:26 +0100

    testing commit

--- app/src/main/java/mprog/simon/urlshortnr/ShortenURLActivity.java
+++ app/src/main/java/mprog/simon/urlshortnr/ShortenURLActivity.java
@@ -1,5 +1,9 @@
 package mprog.simon.urlshortnr;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +12,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class ShortenURLActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ShortenURLActivity extends AppCompatActivity implements ShortenURLAsyncTask.AsyncResponse {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +35,9 @@ public class ShortenURLActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // go to sign in activity
+                Intent intent = new Intent(ShortenURLActivity.this, LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
@@ -43,10 +57,85 @@ public class ShortenURLActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_settings:
+                return true;
+
+            case R.id.action_search:
+                // go to look up activity
+                Intent intent = new Intent(this, LookupURLActivity.class);
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //this override the implemented method from AsyncResponse
+    @Override
+    public void processFinish(JSONObject output){
+        String shortUrl = null;
+        try {
+            shortUrl = output.getString("id");
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
 
-        return super.onOptionsItemSelected(item);
+        TextView urlView = (TextView) findViewById(R.id.shortUrlTV);
+        urlView.setText(shortUrl);
+        urlView.setVisibility(View.VISIBLE);
+    }
+
+    public void shortenUrl(View view) {
+        // Hide url display
+        TextView urlView = (TextView) findViewById(R.id.shortUrlTV);
+        urlView.setVisibility(View.INVISIBLE);
+
+        // get url to shorten
+        EditText urlTextView = (EditText) findViewById(R.id.urlEditText);
+        String url = urlTextView.getText().toString();
+
+        // do not start async short url lookup if query is not a valid url
+        if (!isValidUrl(url)) {
+            Toast.makeText(this, "Please provide a valid URL", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // create asyncTask class instance and execute
+        ShortenURLAsyncTask asyncTask = new ShortenURLAsyncTask(this, this);
+        asyncTask.execute(url);
+    }
+
+    public void copyUrlToClipboard(View view) {
+        TextView urlTextView = (TextView) view;
+        // return if no url was looked up
+        if (urlTextView.getVisibility() == View.INVISIBLE) {
+            return;
+        }
+
+        // get url from text view
+        String url = urlTextView.getText().toString();
+
+        // add url to clipboard
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("Short URL", url);
+        clipboard.setPrimaryClip(clip);
+
+        Toast.makeText(this, "Copied URL to clipboard", Toast.LENGTH_SHORT).show();
+    }
+
+    /** A helper function that validates a url on a very basic level **/
+    public boolean isValidUrl(String urlString) {
+
+        // try to form a url from the string, if succesful return true
+        try {
+            new URL(urlString);
+        } catch (MalformedURLException e) {
+            return false;
+        }
+
+        return true;
     }
 }
--- app/src/main/java/mprog/simon/urlshortnr/ShortenURLAsyncTask.java
+++ app/src/main/java/mprog/simon/urlshortnr/ShortenURLAsyncTask.java
@@ -2,10 +2,21 @@ package mprog.simon.urlshortnr;
 
 import android.app.Activity;
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.Console;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * An AsyncTask Class to fetch a shortened URL from the goo.gl API
@@ -13,7 +24,7 @@ import org.json.JSONObject;
 
 public class ShortenURLAsyncTask extends AsyncTask<String, Void, String> {
 
-    private AsyncResponse delegate = null;
+    public AsyncResponse delegate = null;
     private Activity mActivity;
 
     public ShortenURLAsyncTask(Activity activity, AsyncResponse delegate){
@@ -23,7 +34,7 @@ public class ShortenURLAsyncTask extends AsyncTask<String, Void, String> {
 
     // Interface to relay data back to ShortenURLActivity
     public interface AsyncResponse {
-        void processFinish(String output);
+        void processFinish(JSONObject output);
     }
 
     protected void onPreExecute() {
@@ -32,31 +43,87 @@ public class ShortenURLAsyncTask extends AsyncTask<String, Void, String> {
     }
 
     @Override
-    protected String doInBackground(String... strings) {
-        return null;
+    protected String doInBackground(String... args) {
+
+        // api URL including app specific api key
+        String api_url = "https://www.googleapis.com/urlshortener/v1/url?" +
+                "key=AIzaSyDy0G2e5RPk0FwiZ8d4KBEpe8bggh3G3Uk";
+
+        URL url = null;
+        try {
+            // create url
+            url = new URL(api_url);
+
+            // open connection
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection)con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            // create POST request body
+            // body JSON as string (inserting the searched URL, removing space chars)
+            String outString = "{\"longUrl\":\"" + args[0] + "\"}";
+            // convert outstring to byte array
+            byte[] out = outString.getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            http.connect();
+            try(OutputStream os = http.getOutputStream()) {
+                os.write(out);
+            }
+
+            int responseCode = http.getResponseCode();
+            //Toast.makeText(mActivity, Integer.toString(responseCode), Toast.LENGTH_SHORT).show();
+
+
+            // handle response if OK
+            if (responseCode == 200) {
+                try {
+                    // get json data as string
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(http.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+
+                } finally {
+                    http.disconnect();
+                }
+            }
+            // if response is bad, disconnect and relay error message
+            else {
+                http.disconnect();
+                Log.e("ERROR", Integer.toString(responseCode));
+                return "ERROR " + Integer.toString(responseCode);
+            }
+        } catch (Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return "ERROR shortening URL";
+        }
     }
 
     protected void onPostExecute(String result) {
-        if (result == null) {
+        // A space can never be in a correct link, therefor it must indicate an error
+        // show error message to user if shortening the link failed
+        if (result.contains("ERROR ")) {
+            Toast.makeText(mActivity, result, Toast.LENGTH_SHORT).show();
             return;
         }
 
         // JSON-ify the result string
-        // handle error returns from omdb
-        JSONObject movieInfo = null;
+        // and delegate to ShortenURLActivity
+        JSONObject resultJSON = null;
         try {
-            movieInfo = new JSONObject(result);
-            if (!movieInfo.getBoolean("Response")) {
-                Toast.makeText(mActivity, movieInfo.getString("Error"), Toast.LENGTH_LONG).show();
-            }
-            else {
-                Toast.makeText(mActivity.getApplicationContext(), "Fetched data",
-                        Toast.LENGTH_SHORT).show();
-
-                delegate.processFinish(result);
-            }
+            resultJSON = new JSONObject(result);
+            delegate.processFinish(resultJSON);
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
     }
 }
